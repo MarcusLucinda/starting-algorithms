3d7c38ccb0cf8a267e781b489111409530f64b00
MarcusLucinda <marcusvbl@hotmail.com>
2021-05-16 20:37:34 -0300

    updated binary search

--- binary_search.py
+++ binary_search.py
@@ -3,7 +3,7 @@ def binary_search(list, item):
     high = (len(list) - 1)
 
     while(low <= high):
-        mid = (low + high)
+        mid = (low + high) // 2
         guess = list[mid]
         if(guess == item):
             return mid
@@ -14,6 +14,6 @@ def binary_search(list, item):
     return None
 
 
-my_list = [1, 3, 5, 7, 9]
-print(binary_search(my_list, 7))
+my_list = [1, 2, 3, 5, 6, 7, 8, 9, 10]
+print(binary_search(my_list, 3))
 print(binary_search(my_list, 4))
